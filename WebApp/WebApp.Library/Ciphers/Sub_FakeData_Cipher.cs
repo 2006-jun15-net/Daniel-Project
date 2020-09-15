61b83ba524918368fe9350f3f327d0bdaf71d024
Daniel Aasa <danielaasa42@gmail.com>
2020-09-15 13:11:53 -0700

    Finished Sub_FakeData_Cipher

--- WebApp/WebApp.Library/Ciphers/Sub_FakeData_Cipher.cs
+++ WebApp/WebApp.Library/Ciphers/Sub_FakeData_Cipher.cs
@@ -4,7 +4,126 @@ using System.Text;
 
 namespace WebApp.Library.Ciphers
 {
-    class Sub_FakeData_Cipher
+    public class Sub_FakeData_Cipher
     {
+        public string printableCharacters = " !" + '"' + "#$%&'()*+,-./0123456789" +
+            ":;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[" + "\\" + "]^_`" +
+            "abcdefghijklmnopqrstuvwxyz{|}~";
+
+        public string Encode(string x, int a, int b)
+        {
+            int printLength = printableCharacters.Length;
+            int inputLength = x.Length;
+            int c = (int)Math.Pow(b, a);
+
+            int amountOfFakeData = (int)((inputLength / a) + (inputLength / c));
+
+            int total = inputLength + amountOfFakeData;
+
+            char[] letters = new char[total];
+
+            int count = 0;
+            bool toggle = false;
+            int index = 0;
+
+            for (int i = 0; i < inputLength; i++)
+            {
+                letters[count] = x[i];
+
+
+                count++;
+
+                if (((i+1) % a) == 0)
+                {
+                    if (toggle == false)
+                    {
+                        index += (a * b);
+                        index %= printLength;
+                        letters[count] = printableCharacters[index];
+                    }
+                    if (toggle == true)
+                    {
+                        index += b;
+                        index %= printLength;
+                        letters[count] = printableCharacters[index];
+                    }
+                    count++;
+                    toggle = !toggle;
+                }
+                if (((i+1) % c) == 0){
+                    if (toggle == false)
+                    {
+                        index += (a * b);
+                        index %= printLength;
+                        letters[count] = printableCharacters[index];
+                    }
+                    if (toggle == true)
+                    {
+                        index += b;
+                        index %= printLength;
+                        letters[count] = printableCharacters[index];
+                    }
+                    count++;
+                    toggle = !toggle;
+                }
+            }
+
+
+
+            string result = "";
+
+            foreach(char i in letters)
+            {
+                result += i;
+            }
+
+            return result;
+        }
+
+        public string Decode(string x, int a, int b)
+        {
+            int inputLength = x.Length;
+            int c = (int)Math.Pow(b, a);
+
+            char[] letters = new char[inputLength];
+
+            int count = 0;
+
+            for (int i = 0; i < inputLength; i++)
+            {
+                if(count >= inputLength)
+                {
+                    break;
+                }
+                letters[i] = x[count];
+
+
+                count++;
+
+                if (((i + 1) % a) == 0)
+                {
+                    count++;
+                }
+                if (((i + 1) % c) == 0)
+                {
+                    count++;
+                }
+            }
+
+
+
+            string result = "";
+
+            foreach (char i in letters)
+            {
+                if(i == '\0')
+                {
+                    break;
+                }
+                result += i;
+            }
+
+            return result;
+        }
     }
 }
---
+++ WebApp/WebApp.Test/Ciphers.Test/Sub_FakeData_Cipher_Test.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using WebApp.Library.Ciphers;
+using Xunit;
+
+namespace WebApp.Test.Ciphers.Test
+{
+    public class Sub_FakeData_Cipher_Test
+    {
+        [Fact]
+        public void Encode_smallString()
+        {
+            string input = "walking";
+            int a = 3;
+            int b = 2;
+
+            string expected = "wal&kin(g";
+
+            var fakeData = new Sub_FakeData_Cipher();
+
+            Assert.Equal(expected, fakeData.Encode(input, a, b));
+        }
+
+        [Fact]
+        public void Encode_bigString()
+        {
+            string input = "a man walking his dog at the park";
+            int a = 5;
+            int b = 2;
+
+            string expected = "a man* walk,ing h6is do8g at Bthe pDarNk";
+
+            var fakeData = new Sub_FakeData_Cipher();
+
+            Assert.Equal(expected, fakeData.Encode(input, a, b));
+        }
+
+        [Fact]
+        public void Decode_bigString()
+        {
+            string input = "a man* walk,ing h6is do8g at Bthe pDarNk";
+            int a = 5;
+            int b = 2;
+
+            string expected = "a man walking his dog at the park";
+
+            var fakeData = new Sub_FakeData_Cipher();
+
+            Assert.Equal(expected, fakeData.Decode(input, a, b));
+        }
+
+        [Fact]
+        public void Decode_smallString()
+        {
+            string input = "wal&kin(g";
+            int a = 3;
+            int b = 2;
+
+            string expected = "walking";
+
+            var fakeData = new Sub_FakeData_Cipher();
+
+            Assert.Equal(expected, fakeData.Decode(input, a, b));
+        }
+    }
+}
