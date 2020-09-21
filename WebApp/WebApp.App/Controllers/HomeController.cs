2b397acd7e9c4f7a67e59d73d3c3ec7fbbb1bb9c
Daniel Aasa <danielaasa42@gmail.com>
2020-09-21 09:29:23 -0700

    mvc setup part 1

--- WebApp/WebApp.App/Controllers/HomeController.cs
+++ WebApp/WebApp.App/Controllers/HomeController.cs
@@ -6,21 +6,48 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApp.App.Models;
+using WebApp.Library.Ciphers;
 
 namespace WebApp.App.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly Shift_Cipher _shift_Cipher;
+        public HomeController(ILogger<HomeController> logger, Shift_Cipher shift_Cipher)
         {
+            _shift_Cipher = shift_Cipher ?? throw new ArgumentNullException(nameof(shift_Cipher));
             _logger = logger;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Encode(TextBoxValue text)
+        {
+            string Cipher = text.Cipher(text.Key);
+            
+            //Shift_Cipher
+            //if (Cipher[0] == 'A')
+           // {
+                int shift = text.Shift_Cipher_Number(Cipher);
+
+                //add logic for repeat of the cypher
+                string nkey = Cipher;
+                string _value = _shift_Cipher.Encode(text.Value, shift);
+                nkey += $"A{shift}";
+            // }
+
+            //temp
+            nkey = text.Key + "A4";
+            //finish
+            var newText = new TextBoxValue() { Value = _value, Key = nkey };
+            ModelState.Clear();
+
+            return View("Index", newText);
         }
 
         public IActionResult Privacy()
---
+++ WebApp/WebApp.App/Models/TextBoxValue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.App.Models
+{
+    public class TextBoxValue
+    {
+        public string CipherID { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        [Required]
+        public string Key { get; set; }
+
+        public string Num1 { get; set; }
+
+        public string Num2 { get; set; }
+
+        public string EncodeKey { get; set; }
+
+        public string Cipher (string x)
+        {
+            char type = 'A';
+            char num = '0';
+            foreach (char i in x)
+            {
+                if (Char.IsDigit(i) == false)
+                {
+
+                    type = i;
+
+                }
+                if (Char.IsDigit(i) == true)
+                {
+                    num = i;
+                }
+            }
+            
+            return type.ToString() + num.ToString();
+        }
+
+        public int Shift_Cipher_Number(string x)
+        {
+            string number = "";
+            int num = 1;
+            foreach (char i in x)
+            {
+                if(Char.IsDigit(i) == true)
+                {
+                    number += i;
+                }
+            }
+            num = Int32.Parse(number);
+            return num;
+        }
+    }
+}
